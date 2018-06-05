2024a2145954d5282542066c1f98679866cf2b49
Yusuf Yasin <yusufnyasin@gmail.com>
2018-06-05 16:47:11 +0800

    LCT 160. Apply force to Tank.

---
+++ UnrealBattleTank/Source/UnrealBattleTank/Public/TankTrack.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * Tank Track is used to set maximum driving force, and to apply forces.
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class UNREALBATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets a throttles between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 ton tank, and 1g accelleration
+	
+};
